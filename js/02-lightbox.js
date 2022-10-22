8a2f580c8db8a6761ab4b950a44a2cdea5f3f671
artem.karimov <artem.karimov@ecosoft.com>
2022-10-22 23:04:37 +0300

    Finish task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const { SimpleLightbox } = window;
+
+const gallery = document.querySelector('.gallery');
+
+const displayImages = () => {
+  const items = [];
+
+  for (const galleryItem of galleryItems) {
+    const item = `
+      <a class="gallery__item" href="${galleryItem.original}">
+        <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+      </a>
+    `;
+    items.push(item);
+  }
+
+  gallery.insertAdjacentHTML('afterbegin', items.join('\n'));
+};
+
+displayImages();
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
