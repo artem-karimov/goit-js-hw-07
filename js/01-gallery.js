feb926136827c7e614a745f536ffa1974a2b867f
artem.karimov <artem.karimov@ecosoft.com>
2022-10-22 22:19:21 +0300

    Finish task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const { basicLightbox } = window;
+
+const gallery = document.querySelector('.gallery');
+
+const displayImages = () => {
+  const items = [];
+
+  for (const galleryItem of galleryItems) {
+    const item = `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${galleryItem.original}">
+          <img
+            class="gallery__image"
+            src="${galleryItem.preview}"
+            data-source="${galleryItem.original}"
+            alt="${galleryItem.description}"
+          />
+        </a>
+      </div>
+    `;
+    items.push(item);
+  }
+
+  gallery.insertAdjacentHTML('afterbegin', items.join('\n'));
+};
+
+displayImages();
+
+let openedLightbox;
+
+const handleCloseLightbox = event => {
+  if (event.key === 'Escape') openedLightbox.close();
+};
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+
+  const { target } = event;
+
+  if (target.tagName === 'IMG') {
+    basicLightbox
+      .create(
+        `<img src="${target.getAttribute('data-source')}" alt="${target.getAttribute('alt')}">`,
+        {
+          onShow: instance => {
+            openedLightbox = instance;
+            window.addEventListener('keydown', handleCloseLightbox);
+          },
+          onClose: () => {
+            window.removeEventListener('keydown', handleCloseLightbox);
+          },
+        },
+      )
+      .show();
+  }
+});
